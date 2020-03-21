cfb6ba73c2d6c8dcb99b3e7f67f5d6872493bb41
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-03-21 14:44:49 +0100

    Oppdaterer deprecated

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,26 +36,26 @@ repositories {
 
 dependencies {
     // Server
-    compile("no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-jackson:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-metrics:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-health:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-auth:$dusseldorfKtorVersion")
 
     // Client
-    compile("no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
-    compile("no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
+    implementation("no.nav.helse:dusseldorf-oauth2-client:$dusseldorfKtorVersion")
 
     // Kafka
-    compile("org.apache.kafka:kafka-clients:$kafkaVersion")
+    implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
 
     // Test
-    testCompile("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
-    testCompile("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
-    testCompile("io.ktor:ktor-server-test-host:$ktorVersion") {
+    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
+    testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
-    testCompile("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation("org.skyscreamer:jsonassert:1.5.0")
 }
 
 java {
