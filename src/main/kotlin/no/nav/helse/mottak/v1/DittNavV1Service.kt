9cde30977748c40e0e28070a4a473d39dd46d6d7
Andre Wolden <wolden.andre@gmail.com>
2020-04-15 15:06:26 +0200

    refaktoriserer

--- src/main/kotlin/no/nav/helse/mottak/v1/DittNavV1Service.kt
+++ src/main/kotlin/no/nav/helse/mottak/v1/DittNavV1Service.kt
@@ -1,27 +1,44 @@
 package no.nav.helse.mottak.v1
 
 import no.nav.helse.SoknadId
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+
+private val logger: Logger = LoggerFactory.getLogger("no.nav.DittNavV1Service")
 
 internal class DittNavV1Service(
     private val soknadV1KafkaProducer: SoknadV1KafkaProducer
 ) {
-    private companion object {
-        private val logger = LoggerFactory.getLogger(SoknadV1MottakService::class.java)
-    }
-
     fun sendSoknadMottattMeldingTilDittNav(
         dto: ProduceBeskjedDto,
         søkersNorskeIdent: String,
         soknadId: SoknadId
-    ): String {
-
+    ) {
         soknadV1KafkaProducer.produceDittnavMelding(
             dto = dto,
             søkersNorskeIdent = søkersNorskeIdent,
             soknadId = soknadId
         )
+    }
+}
 
-        return "Melding sendt til Ditt Nav sin Kafka kø"
+internal fun sendBeskjedTilDittNav(
+    dittNavV1Service: DittNavV1Service,
+    dittNavTekst: String,
+    dittNavLink: String,
+    sokerFodselsNummer: String,
+    soknadId: SoknadId
+) {
+    try {
+        dittNavV1Service.sendSoknadMottattMeldingTilDittNav(
+            dto = ProduceBeskjedDto(
+                tekst = dittNavTekst,
+                link = dittNavLink
+            ),
+            søkersNorskeIdent = sokerFodselsNummer,
+            soknadId = soknadId
+        )
+    } catch (e: Exception) {
+        logger.error("Kunne ikke sende melding til Ditt NAV om innsendt søknad: $e")
     }
 }
--- src/main/kotlin/no/nav/helse/mottak/v1/SoknadV1Api.kt
+++ src/main/kotlin/no/nav/helse/mottak/v1/SoknadV1Api.kt
@@ -6,7 +6,6 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.ApplicationRequest
 import io.ktor.request.header
-import io.ktor.request.receiveChannel
 import io.ktor.request.receiveStream
 import io.ktor.response.ApplicationResponse
 import io.ktor.response.respond
@@ -34,17 +33,13 @@ internal fun Route.SoknadV1Api(
             metadata = metadata,
             soknad = soknad
         )
-        try {
-            dittNavV1Service.sendSoknadMottattMeldingTilDittNav(
-                dto = ProduceBeskjedDto(
-                    tekst = "Vi har mottatt søknaden din om pleiepenger. Les mer om hva som skjer etter at du har søkt.",
-                    link = "https://www.nav.no/familie/sykdom-i-familien/nb/pleiepenger-for-sykt-barn#Etter-at-du-har-sokt"),
-                søkersNorskeIdent = soknad.sokerFodselsNummer,
-                soknadId = soknadId
-            )
-        } catch (e: Exception) {
-            logger.error("Kunne ikke sende melding til Ditt NAV om innsendt søknad: $e")
-        }
+        sendBeskjedTilDittNav(
+            dittNavV1Service = dittNavV1Service,
+            dittNavTekst = "Søknad om utbetaling av omsorgsdager er mottatt.",
+            dittNavLink = "",
+            sokerFodselsNummer = soknad.sokerFodselsNummer,
+            soknadId = soknadId
+        )
         call.respond(HttpStatusCode.Accepted, mapOf("id" to soknadId.id))
     }
 }
